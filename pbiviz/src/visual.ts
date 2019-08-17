62719929189830cc697f6debc7edb56ef3453810
Alok Pepakayala <aloknookraj@gmail.com>
2019-08-17 11:40:19 +0530

    pbiviz folder setup
    Created a new custom visual using pbiviz new and removed few files such as settings and style as those are managed in the module directly. To ensure the package is building, had to add some metadata to pbiviz.json

---
+++ pbiviz/src/visual.ts
@@ -0,0 +1,49 @@
+"use strict";
+
+import "core-js/stable";
+import powerbi from "powerbi-visuals-api";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualObjectInstance = powerbi.VisualObjectInstance;
+import DataView = powerbi.DataView;
+import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+
+export class Visual implements IVisual {
+    private target: HTMLElement;
+    private updateCount: number;
+    private textNode: Text;
+
+    constructor(options: VisualConstructorOptions) {
+        console.log('Visual constructor', options);
+        this.target = options.element;
+        this.updateCount = 0;
+        if (typeof document !== "undefined") {
+            const new_p: HTMLElement = document.createElement("p");
+            new_p.appendChild(document.createTextNode("Update count:"));
+            const new_em: HTMLElement = document.createElement("em");
+            this.textNode = document.createTextNode(this.updateCount.toString());
+            new_em.appendChild(this.textNode);
+            new_p.appendChild(new_em);
+            this.target.appendChild(new_p);
+        }
+    }
+
+    public update(options: VisualUpdateOptions) {
+        console.log('Visual update', options);
+        if (typeof this.textNode !== "undefined") {
+            this.textNode.textContent = (this.updateCount++).toString();
+        }
+    }
+
+
+    /**
+     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
+     * objects and properties you want to expose to the users in the property pane.
+     *
+     */
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
+        return;
+    }
+}
