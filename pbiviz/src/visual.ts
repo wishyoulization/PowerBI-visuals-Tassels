f68a8278549668a757aec63bdf54233e7131860f
Alok Pepakayala <aloknookraj@gmail.com>
2019-08-17 14:30:31 +0530

    MVP reached
    A working parallel sets custom visual for power BI that we can drag and drop up to 10 columns and a measure to play around with the parellel sets interactively.

--- pbiviz/src/visual.ts
+++ pbiviz/src/visual.ts
@@ -2,39 +2,101 @@
 
 import "core-js/stable";
 import powerbi from "powerbi-visuals-api";
-import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
-import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import '../../build/App';
+import * as models from 'powerbi-models';
+
+
+import IColorPalette = powerbi.extensibility.IColorPalette;
+import IViewport = powerbi.IViewport;
 import IVisual = powerbi.extensibility.visual.IVisual;
-import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
-import VisualObjectInstance = powerbi.VisualObjectInstance;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
+
 import DataView = powerbi.DataView;
+import DataViewCategoryColumn = powerbi.DataViewCategoryColumn;
+import DataViewMetadataColumn = powerbi.DataViewMetadataColumn;
+import DataViewValueColumn = powerbi.DataViewValueColumn;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualObjectInstance = powerbi.VisualObjectInstance;
+import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+import VisualObjectInstancesToPersist = powerbi.VisualObjectInstancesToPersist;
+import VisualTooltipDataItem = powerbi.extensibility.VisualTooltipDataItem;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+
 
 export class Visual implements IVisual {
-    private target: HTMLElement;
-    private updateCount: number;
-    private textNode: Text;
+    private host: IVisualHost;
+    private colorPalette: IColorPalette;
 
     constructor(options: VisualConstructorOptions) {
-        console.log('Visual constructor', options);
-        this.target = options.element;
-        this.updateCount = 0;
-        if (typeof document !== "undefined") {
-            const new_p: HTMLElement = document.createElement("p");
-            new_p.appendChild(document.createTextNode("Update count:"));
-            const new_em: HTMLElement = document.createElement("em");
-            this.textNode = document.createTextNode(this.updateCount.toString());
-            new_em.appendChild(this.textNode);
-            new_p.appendChild(new_em);
-            this.target.appendChild(new_p);
+        this.host = options.host;
+        //Create the app element container and app element to embed the visual module
+        let appContainer = document.createElement("div");
+        let app = document.createElement("div");
+        appContainer.setAttribute("id", "appContainer");
+        appContainer.style.width = "100%";
+        appContainer.style.height = "100%";
+        appContainer.style.overflow = "auto";
+        app.setAttribute("id", "app");
+        appContainer.appendChild(app);
+        options.element.appendChild(appContainer);
+    }
+
+
+    private getObjectFromDataView(dataView: DataView) {
+        let metadata = [];
+
+        let columns = dataView.table.columns
+        for (let c = 0; c < columns.length; c++) {
+            let currentColumn = columns[c];
+            if (currentColumn.isMeasure) {
+                metadata.push({
+                    column: null,
+                    table: null,
+                    name: currentColumn.displayName,
+                    roles: Object.keys(currentColumn.roles)
+                });
+
+            } else {
+                metadata.push({
+                    column: currentColumn.queryName.replace(/.*\./, ""),
+                    table: currentColumn.queryName.replace(/\..*/, ""),
+                    name: currentColumn.displayName,
+                    roles: Object.keys(currentColumn.roles)
+                });
+            }
         }
+
+
+        return {
+            metadata: metadata,
+            rows: dataView.table.rows,
+
+            tempData: dataView.table.rows.map(function (row) {
+                let t = {};
+                row.map(function (d, i) {
+                    let m = metadata[i];
+                    let n = m.name;
+                    if (m.table === null) {
+                        n = "___VALUE___";
+                    }
+                    t[n] = d
+                })
+                return t;
+            })
+        };
     }
 
     public update(options: VisualUpdateOptions) {
-        console.log('Visual update', options);
-        if (typeof this.textNode !== "undefined") {
-            this.textNode.textContent = (this.updateCount++).toString();
+        if (!options
+            || !options.dataViews
+            || !options.dataViews[0]
+        ) {
+            return;
         }
+        console.log('Visual update', options);
+        (window as any).CustomVisualManager(this.getObjectFromDataView(options.dataViews[0]).tempData);
     }
 
 
